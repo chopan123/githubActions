e194c34d35b92adf249579f82ffc746ae4dafd34
chopan123 <chopancatrileo@gmail.com>
2022-05-23 11:23:43 -0400

    Post with curl

--- postGithubAction.sh
+++ postGithubAction.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Make sure GITHUB_PAT is defined
 
 curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
+  -u "chopan123:$GITHUB_PAT" \
   https://api.github.com/repos/chopan123/githubActions/dispatches \
   -d '{"event_type":"build","client_payload":{"unit":false,"integration":true}}'
+
+  # curl \
+  #   -X POST \
+  #   -H "Accept: application/vnd.github.v3+json" \
+  #   -H "Authorization: token $GITHUB_PAT" \
+  #   https://api.github.com/repos/chopan123/githubActions/dispatches \
+  #   -d '{"event_type":"build","client_payload":{"unit":false,"integration":true}}'
